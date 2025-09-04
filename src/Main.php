35a76f2b374648476f2e16bf888993f7ec7b335f
Floxy <142106680+floxy0@users.noreply.github.com>
2025-09-04 18:02:14 +0200

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -5,13 +5,19 @@ use pocketmine\player\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Server;
+use pocketmine\network\mcpe\protocol\TransferPacket;
 
 class Main extends PluginBase{
+ public function onEnable(): void{
+  $this->saveDefaultConfig();
+ }
  public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
   if(strtolower($command->getName())==="restart"){
    if($sender instanceof Player && !$sender->hasPermission("betterserver.restart")) return true;
+   $ip = $this->getConfig()->get("restart-ip","0.0.0.0");
+   $port = (int)$this->getConfig()->get("restart-port",19132);
    foreach(Server::getInstance()->getOnlinePlayers() as $player){
-    $player->chat("/reconnect");
+    $player->getNetworkSession()->sendDataPacket(TransferPacket::create($ip,$port,false));
    }
    $this->getServer()->shutdown();
    return true;
